9d4eae6ef1c835847051f7cd12af1ac6f45fc3bc
Elise <eal109@georgetown.edu>
2020-12-01 10:37:26 -0500

    EL: removed some extraneous functions, added comments for each function

--- code/gam_fx.R
+++ code/gam_fx.R
@@ -1,180 +1,27 @@
+#phenology & lag functions for survey and incidental data
+
 #if(!requireNamespace("devtools")) install.packages("devtools")
 #devtools::install_github("RetoSchmucki/rbms")
 require(rbms)
-#CI function
 
+#butterfly_day function estimates annual abundance at specific site(s) 
+#butterfly_day function input is site_year_sp_count from GAM flight curve phenology
 butterfly_day <- function(site_year_sp_count, WeekCount = TRUE){
   if (WeekCount == TRUE){
     b_day <- site_year_sp_count$impute_count[COMPLT_SEASON == 1 & M_SEASON != 0 & WEEK_DAY == 4, FITTED, by = .(SITE_ID, M_YEAR, WEEK)][,sum(FITTED), by = .(SITE_ID, M_YEAR)]
   } else {
     b_day <- site_year_sp_count$impute_count[COMPLT_SEASON == 1 & M_SEASON != 0, FITTED, by = .(SITE_ID, M_YEAR, WEEK)][,sum(FITTED), by = .(SITE_ID, M_YEAR)]
   }
-
   data.table::setnames(b_day,"V1","BUTTERFLY_DAY")
-
   return(b_day)
 }
 
 
-phenometric<-function(visit, count, indices) {
-  #format visit and count data
-  visit<-visit[indices,]
-  #count<-tempdata[[2]]
-  count<-count[count$SITE_ID%in%visit$SITE_ID & count$DATE%in%visit$DATE,]
-  ts_date <- rbms::ts_dwmy_table(InitYear = as.numeric(format(visit$DATE[1],"%Y")), LastYear = as.numeric(format(visit$DATE[1],"%Y")), WeekDay1 = 'monday')
-
-  ts_season <- rbms::ts_monit_season(ts_date, StartMonth = 2, EndMonth = 10, StartDay = 1, EndDay = NULL,
-                               CompltSeason = TRUE, Anchor = TRUE, AnchorLength = 7, AnchorLag = 7)
-  ts_season_visit <- rbms::ts_monit_site(visit, ts_season)
-
-  ts_season_count <- rbms::ts_monit_count_site(ts_season_visit, count, sp = count$SPECIES[1])
-
-  ## compute the flight curve, using the
-  ## regionalGAM method
-  ##=========================================
-  dataset_y <- ts_season_count[, .(SPECIES, SITE_ID, DATE, WEEK, WEEK_DAY, DAY_SINCE, M_YEAR, M_SEASON, COUNT, ANCHOR)]
-  dataset_y[, trimDAYNO := DAY_SINCE - min(DAY_SINCE) + 1]
-
-  system.time(
-    ts_flight_curve <- rbms::flight_curve(ts_season_count, NbrSample = 200, MinVisit = 2, MinOccur = 1, MinNbrSite = 1,
-                                    MaxTrial = 3, FcMethod = 'regionalGAM', GamFamily = 'nb', SpeedGam = FALSE, CompltSeason = TRUE)
-  )
-
-  #check if successful
-  if(is.na(max(ts_flight_curve$pheno[, NM]))){ }else {
-
-    ## Calculate pheno metrics
-    site_year_sp_count <- impute_count(ts_season_count, ts_flight_curve$pheno,  FamilyGlm = 'quasipoisson') #SpeedGlm = FALSE,
-
-  site_sp_yr_abund<-butterfly_day(site_year_sp_count)
-
-  test1<-site_year_sp_count$impute_count %>%
-    group_by(SITE_ID, M_YEAR) %>%
-    mutate(bd=cumsum(NM))
-
-  ##onset
-  #test1[test1$bd>0,c(3,4,15,16,19)][1,]
-  #10% curve
-  p10<-test1[test1$bd>0.09999,c(3,4,15,16,19)][1,3][[1]]
-  #50% curve
-  p50<-test1[test1$bd>0.49999,c(3,4,15,16,19)][1,3][[1]]
-
-  } #end if regional GAM was successful
-  return(c(p10,p50))
-} # end if data density sufficient
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-phenometric2<-function(sites, visit, cnt, indices) {
-  #format visit and count data
-  sites<-sites[indices]
-  visit<-visit[visit$SITE_ID%in%sites,]
-  cnt<-cnt[cnt$SITE_ID%in%sites,]
-
-  ts_date <- rbms::ts_dwmy_table(InitYear = as.numeric(format(visit$DATE[1],"%Y")), LastYear = as.numeric(format(visit$DATE[1],"%Y")), WeekDay1 = 'monday')
-
-  ts_season <- rbms::ts_monit_season(ts_date, StartMonth = 4, EndMonth = 10, StartDay = 1, EndDay = NULL,
-                               CompltSeason = TRUE, Anchor = TRUE, AnchorLength = 7, AnchorLag = 7)
-  ts_season_visit <- rbms::ts_monit_site(visit, ts_season)
-
-  ts_season_count <- rbms::ts_monit_count_site(ts_season_visit, cnt, sp = cnt$SPECIES[1])
-
-  ## compute the flight curve, using the
-  ## regionalGAM method
-  ##=========================================
-  dataset_y <- ts_season_count[, .(SPECIES, SITE_ID, DATE, WEEK, WEEK_DAY, DAY_SINCE, M_YEAR, M_SEASON, COUNT, ANCHOR)]
-  dataset_y[, trimDAYNO := DAY_SINCE - min(DAY_SINCE) + 1]
-
-  system.time(
-    ts_flight_curve <- rbms::fit_gam(dataset_y, NbrSample = 200, GamFamily = 'nb', MaxTrial = 3)
-
-    #  flight_curve(ts_season_count, NbrSample = 200, MinVisit = 5, MinOccur = 3, MinNbrSite = 2,
-    #                                MaxTrial = 3, FcMethod = 'regionalGAM', GamFamily = 'nb', SpeedGam = FALSE, CompltSeason = TRUE)
-  )
-
-
-  #check if successful
-  ts_flight_curve$pheno<-ts_flight_curve$f_curve
-  if(is.na(max(ts_flight_curve$pheno[, NM]))){ }else {
-
-    ## Calculate pheno metrics
-    #ts1<-ts_flight_curve$pheno%>%
-    #  group_by(SPECIES, DATE, WEEK, trimDAYNO) %>%
-    #  NM=max(NM)
-    #ts1<-data.frame(ts1)
-    ts1<-aggregate(ts_flight_curve$pheno, by = list(ts_flight_curve$pheno$SPECIES, ts_flight_curve$pheno$DATE, ts_flight_curve$pheno$WEEK, ts_flight_curve$pheno$WEEK_DAY, ts_flight_curve$pheno$DAY_SINCE, ts_flight_curve$pheno$M_YEAR, ts_flight_curve$pheno$M_SEASON,ts_flight_curve$pheno$trimDAYNO),FUN = max)
-    site_year_sp_count<- impute_count(ts_season_count, ts_flight_curve$pheno,  FamilyGlm = 'quasipoisson') #SpeedGlm = FALSE,
-
-    site_sp_yr_abund<-butterfly_day(site_year_sp_count)
-
-    test1<-site_year_sp_count$impute_count %>%
-      group_by(SITE_ID, M_YEAR) %>%
-      mutate(bd=cumsum(NM))
-
-    ##onset
-    #test1[test1$bd>0,c(3,4,15,16,19)][1,]
-    #10% curve
-    p10<-test1[test1$bd>0.09999,c(3,4,15,16,19)][1,3][[1]]
-    #50% curve
-    p50<-test1[test1$bd>0.49999,c(3,4,15,16,19)][1,3][[1]]
-
-  } #end if regional GAM was successful
-  return(c(p10,p50))
-} # end if data density sufficient
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-phenometric3<-function(sites, visit, cnt, indices) {
+#phenoFlight function estimates flight curve phenology & returns specific phenometrics
+#Phenometrics currently hardcoded but should be functionalized
+#Assumes abundance data from repeated surveys to specific site(s)
+#Assumes phenology is consistent across sites, abundance varies
+fcPhenometrics<-function(sites, visit, cnt, indices) {
   #format visit and count data
   sites<-sites[indices]
   visit<-visit[visit$DATE%in%sites,]
@@ -199,22 +46,12 @@ phenometric3<-function(sites, visit, cnt, indices) {
 
     ts_flight_curve <- rbms::flight_curve(ts_season_count, NbrSample = 200, MinVisit = 4, MinOccur = 4, MinNbrSite = 1,
                                   MaxTrial = 3, FcMethod = 'regionalGAM', GamFamily = 'nb', SpeedGam = FALSE, CompltSeason = TRUE)
-  )
-  )
-
+  )   )
 
   #check if successful
   if(is.na(max(ts_flight_curve$pheno[, NM]))){ }else {
 
-    ## Calculate pheno metrics
-    #site_year_sp_count <- impute_count(ts_season_count, ts_flight_curve$pheno,  FamilyGlm = 'quasipoisson') #SpeedGlm = FALSE,
-
-
-    #ts1<-ts_flight_curve$pheno%>%
-    #  group_by(SPECIES, DATE, WEEK, trimDAYNO) %>%
-    #  NM=max(NM)
-    #ts1<-data.frame(ts1)
-    test1<-ts_flight_curve$pheno %>%
+   test1<-ts_flight_curve$pheno %>%
       group_by(M_YEAR) %>%
       mutate(bd=cumsum(NM))
 
@@ -234,27 +71,10 @@ phenometric3<-function(sites, visit, cnt, indices) {
 } # end if data density sufficient
 
 
-
-
-
-
-
-
-
-
-phenometric4<-  function (sites, visit, cnt, indices) {
-    out<-c(0,0)
-    out <- tryCatch(phenometric3(sites, visit, cnt, indices), error = function(e) NULL)
-    return(out)
-  }
-
-  tryCatch(phenometric3)
-
-
-
-
-
-phenometric3b<-function(index, visit, cnt, indices) {
+#fcPhenoboot function also estimates flight curve phenometrics from abundance data
+#assumes repeated surveys at specific site(s)
+#allows bootstrapping
+fcPhenoboot<-function(index, visit, cnt, indices) {
   #format visit and count data
   ind<-index[indices]
   visit<-visit[visit$DATE%in%ind,]
@@ -275,25 +95,14 @@ phenometric3b<-function(index, visit, cnt, indices) {
     dataset_y[, trimDAYNO := DAY_SINCE - min(DAY_SINCE) + 1]
     try(
       system.time(
-        #ts_flight_curve <- fit_gam(dataset_y, NbrSample = 200, GamFamily = 'nb', MaxTrial = 3)
-
         ts_flight_curve <- flight_curve(ts_season_count, NbrSample = 200, MinVisit = 4, MinOccur = 3, MinNbrSite = 1,
                                         MaxTrial = 3, FcMethod = 'regionalGAM', GamFamily = 'nb', SpeedGam = FALSE, CompltSeason = TRUE)
       )
     )
 
-
     #check if successful
     if(is.na(max(ts_flight_curve$pheno[, NM]))){ }else {
 
-      ## Calculate pheno metrics
-      #site_year_sp_count <- impute_count(ts_season_count, ts_flight_curve$pheno,  FamilyGlm = 'quasipoisson') #SpeedGlm = FALSE,
-
-
-      #ts1<-ts_flight_curve$pheno%>%
-      #  group_by(SPECIES, DATE, WEEK, trimDAYNO) %>%
-      #  NM=max(NM)
-      #ts1<-data.frame(ts1)
       test1<-ts_flight_curve$pheno %>%
         group_by(M_YEAR) %>%
         mutate(bd=cumsum(NM))
@@ -314,8 +123,11 @@ phenometric3b<-function(index, visit, cnt, indices) {
 } # end if data density sufficient
 
 
-
-### FLIGHT CURVE
+#flightcurve funtion estimates phenology/abundance flight curve from abundance data
+#flight curve assumes repeated visit to specific site(s)
+#flightcurve uses a GAM model to smooth abundances over time
+#phenology is assumed consistent across sites, abundance varies across sites
+#flight curve returns relative abundance over time 
 flightcurve<-function(mydata) {
   visit<-mydata %>%
     group_by(SITE_ID, DATE) %>%
@@ -344,8 +156,8 @@ flightcurve<-function(mydata) {
 return(ts_flight_curve$pheno$NM)
 }
 
-
-flightcurve_bootstrap<-function(mydata, indices) {
+#flightcurveBoot allows the flight curve funtion to be bootstrapped
+flightcurveBoot<-function(mydata, indices) {
   mydata<-mydata[indices,]
   visit<-mydata %>%
     group_by(SITE_ID, DATE) %>%
@@ -370,59 +182,15 @@ flightcurve_bootstrap<-function(mydata, indices) {
     ts_flight_curve <- rbms::flight_curve(ts_season_count, NbrSample = 200, MinVisit = 2, MinOccur = 1, MinNbrSite = 1,
                                           MaxTrial = 3, FcMethod = 'regionalGAM', GamFamily = 'nb', SpeedGam = FALSE, CompltSeason = TRUE)
   )
-
   return(ts_flight_curve$pheno$NM)
 }
 
 
-pheno_bootstrap<-function(mydata, indices) {
-  mydata<-mydata[indices,]
-  visit<-mydata %>%
-    group_by(SITE_ID, DATE) %>%
-    select(SITE_ID, DATE)
-  count<-mydata %>%
-    filter(COUNT>0)
-  ts_date <- rbms::ts_dwmy_table(InitYear = as.numeric(format(visit$DATE[1],"%Y")), LastYear = as.numeric(format(visit$DATE[1],"%Y")), WeekDay1 = 'monday')
-
-  ts_season <- rbms::ts_monit_season(ts_date, StartMonth = 2, EndMonth = 10, StartDay = 1, EndDay = NULL,
-                                     CompltSeason = TRUE, Anchor = TRUE, AnchorLength = 7, AnchorLag = 14)
-  ts_season_visit <- rbms::ts_monit_site(visit, ts_season)
-
-  ts_season_count <- rbms::ts_monit_count_site(ts_season_visit, count, sp = count$SPECIES[1])
-
-  ## compute the flight curve, using the
-  ## regionalGAM method
-  ##=========================================
-  dataset_y <- ts_season_count[, .(SPECIES, SITE_ID, DATE, WEEK, WEEK_DAY, DAY_SINCE, M_YEAR, M_SEASON, COUNT, ANCHOR)]
-  dataset_y[, trimDAYNO := DAY_SINCE - min(DAY_SINCE) + 1]
-
-  system.time(
-    ts_flight_curve <- rbms::flight_curve(ts_season_count, NbrSample = 200, MinVisit = 2, MinOccur = 1, MinNbrSite = 1,
-                                          MaxTrial = 3, FcMethod = 'regionalGAM', GamFamily = 'nb', SpeedGam = FALSE, CompltSeason = TRUE)
-  )
-
-  test1<-ts_flight_curve$pheno %>%
-    group_by(M_YEAR) %>%
-    mutate(bd=cumsum(NM))
-
-  ##onset
-  #test1[test1$bd>0,c(3,4,15,16,19)][1,]
-  #10% curve
-  p10<-test1[test1$bd>0.09999,]$trimDAYNO[1]
-  #50% curve
-  p50<-test1[test1$bd>0.49999,]$trimDAYNO[1]
-  return(c(test1$NM[1:365], p10,p50))
- #end if regional GAM was successful
-
-  }
-
-##end bootstrapped phenometrics
-
-
-
-
-
-
+#lagFits function calculates the adult-larval lag for a species/group by:
+#1 - estimates adult phenology using a GAM 
+#2 - estimates larval phenology using smoothed data density
+#3 - shifts larval phenology by # days in lagRange
+#4 - estimates how well phenology curves match @ each lag using a linear model
 lagFits = function(sci_name,              # scientific name
                    Year,                  # year of comparison
                    doy.scope = 1:366,     # day of year scope for estimating phenology
